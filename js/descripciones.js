447c3c9abbfdadcd06f59cf80ff386eb821f4eeb
tgispert <tomasgispert@gmail.com>
2016-12-08 20:14:17 -0300

    fix en manejo de descripciones

--- js/descripciones.js
+++ js/descripciones.js
@@ -1,4 +1,5 @@
 $(".box-hostel-titulo").click(function(){
   $(this).find(".box-descripcion").toggleClass("hidden");
   $(this).toggleClass("descripcion-activa");
+  $(this).find("i").toggleClass("fa-minus").toggleClass("fa-plus");
 });
