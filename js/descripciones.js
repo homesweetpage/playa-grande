f58467fa51ec33ce3f34c2b066690d59f3fb33b3
tgispert <spfury@gmail.com>
2016-12-11 18:31:13 -0300

    cambios en galeria

--- js/descripciones.js
+++ js/descripciones.js
@@ -1,5 +1,13 @@
 $(".box-hostel-titulo").click(function(){
-  $(this).find(".box-descripcion").toggleClass("hidden");
+  if($(this).hasClass("descripcion-activa")){
+    $(this).find(".box-descripcion").addClass("hidden");
+    $(this).find("i").removeClass("fa-minus").addClass("fa-plus");
+  }
+  else {
+    $(".box-hostel-desc").find(".box-descripcion").addClass("hidden");
+    $(".box-hostel-desc").find("i").removeClass("fa-minus").addClass("fa-plus");
+    $(this).find("i").addClass("fa-minus");
+    $(this).find(".box-descripcion").toggleClass("hidden");
+  }
   $(this).toggleClass("descripcion-activa");
-  $(this).find("i").toggleClass("fa-minus").toggleClass("fa-plus");
 });
